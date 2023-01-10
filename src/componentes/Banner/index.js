cba7ab91cfe9b41d06909f4e0103ab34933e2953
Leandro Ferraz <85909017+FerrazLeandro@users.noreply.github.com>
2023-01-10 17:28:08 -0300

    Conclusão do projeto

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/imagens/banner.png" alt="Banner principal da página Organo"/> 
+        </header>
+    )
+}
+
+export default Banner
